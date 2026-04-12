853c2f944343845409b013c01d0853cb0f806a20
Julio <3708774970101@ingenieria.usac.edu.gt>
2026-04-12 11:40:53 -0600

    creacion json para leer y guardar

---
+++ Practica3_API/Services/InventarioService.cs
@@ -0,0 +1,33 @@
+using System.Text.Json;
+using Practica3_API.Models;
+
+namespace Practica3_API.Services
+{
+    public class InventarioService
+    {
+        // Ruta dinámica hacia tu carpeta Data
+        private readonly string _filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "inventario.json");
+
+        // Método para LEER el JSON
+        public List<Producto> GetProductos()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Producto>(); // Si no existe, devolvemos una lista vacía
+            }
+
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Producto>>(json) ?? new List<Producto>();
+        }
+
+        // Método para GUARDAR en el JSON
+        public void SaveProductos(List<Producto> productos)
+        {
+            // WriteIndented = true hace que el JSON se guarde con saltos de línea y se vea bonito, no en una sola línea
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var json = JsonSerializer.Serialize(productos, options);
+            
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
